6d7283edb4f809bc0181a816f8263131dab40ed6
Kouceyla <hadjikouceyla@gmail.com>
2021-10-04 11:32:22 +0100

    fix menu contrast active item

--- src/components/MenuItem.vue
+++ src/components/MenuItem.vue
@@ -2,7 +2,7 @@
   <a
     href="#"
     class="flex justify-between py-3 pl-3 my-1 mr-2 rounded-r-full  hover:bg-cyan-300 transition-all"
-    :class="{'bg-cyan-300': is_current}"
+    :class="{'dark:bg-cyan-600 bg-cyan-400': is_current}"
     >
     <slot></slot>
   </a>
